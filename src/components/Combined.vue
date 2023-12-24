c9b7ae0413f6b24391d95cbd20cfea2a82b97e0b
Nurs <nurrs.serkul@gmail.com>
2023-12-24 20:40:33 +0600

    nav

--- src/components/Combined.vue
+++ src/components/Combined.vue
@@ -13,12 +13,12 @@
         aria-label="Twelfth navbar example"
       >
         <div class="container-fluid container">
-          <a class="navbar-brand" href="#"
+          <router-link to="/" exact class="navbar-brand" href="#"
             ><img
               src="../assets/jihc-logo-png-v1.png"
               alt=""
               style="width: 150px; height: 45px"
-          /></a>
+          /></router-link>
           <button
             class="navbar-toggler"
             type="button"
@@ -34,9 +34,7 @@
             class="collapse navbar-collapse justify-content-md-center"
             id="navbarsExample10"
           >
-            <ul
-              class="navbar-nav justify-content-end flex-grow-1 pe-3 align-self-center d-flex"
-            >
+            <ul class="navbar-nav">
               <li class="nav-item dropdown">
                 <a
                   class="nav-link dropdown-toggle active"
@@ -49,26 +47,58 @@
                 </a>
                 <ul class="dropdown-menu">
                   <li>
-                    <a class="dropdown-item" href="#">История колледжа</a>
-                  </li>
-                  <li><a class="dropdown-item" href="#">Важные факты</a></li>
-                  <li>
-                    <a class="dropdown-item" href="#"
-                      >Лицензии и Аккредитация</a
+                    <router-link
+                      to="/history"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >История колледжа</router-link
                     >
                   </li>
                   <li>
-                    <a class="dropdown-item" href="#">Команда</a>
+                    <router-link
+                      to="/facts"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Важные факты</router-link
+                    >
                   </li>
                   <li>
-                    <a class="dropdown-item" href="#">НПА</a>
+                    <router-link
+                      to="/documents"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Документы</router-link
+                    >
                   </li>
                   <li>
-                    <a class="dropdown-item" href="#">Попечительский совет</a>
+                    <router-link
+                      to="/comand"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Команда</router-link
+                    >
+                  </li>
+
+                  <li>
+                    <router-link
+                      to="/board"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Попечительский совет</router-link
+                    >
                   </li>
                   <li>
-                    <a class="dropdown-item" href="#"
-                      >Лицензии и Аккредитация</a
+                    <router-link
+                      to="/structure"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Структура колледжа</router-link
                     >
                   </li>
                 </ul>
@@ -84,10 +114,32 @@
                   Поступление
                 </a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
                   <li>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <router-link
+                      to="/profession"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Специальности</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/supplydocuments"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Подача документов</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/translate"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Перевод</router-link
+                    >
                   </li>
                 </ul>
               </li>
@@ -102,10 +154,29 @@
                   Специальности
                 </a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
                   <li>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <router-link to="/it" exact class="dropdown-item" href="#"
+                      >Информационные системы <br />
+                      и программное обеспечение</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/accounting"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Бухгалтерcкий учет</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/pedogogika"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Педагогика - математика</router-link
+                    >
                   </li>
                 </ul>
               </li>
@@ -120,10 +191,50 @@
                   Инфраструктура
                 </a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
                   <li>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <router-link
+                      to="/labaratory"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Лаборатории</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/corpus"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Корпус</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/Dormitory"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Общежитие</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/sporthall"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Спортзал + поле</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/canteen"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Столовая</router-link
+                    >
                   </li>
                 </ul>
               </li>
@@ -138,10 +249,77 @@
                   Студентам
                 </a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
                   <li>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <router-link
+                      to="/AcademCalendar"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Академический календарь</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/edupage"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Электронный журнал - Edupage</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/practica"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Практика</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/Career"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Карьера</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/careerguidance"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Проф. ориентация</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/comit"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Комитеты</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/studentlife"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Студенческая жизнь</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/dormitoryrools"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Дом студента</router-link
+                    >
                   </li>
                 </ul>
               </li>
@@ -156,10 +334,32 @@
                   Родителям
                 </a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
                   <li>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <router-link
+                      to="/parentcomit"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Род. комитет</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/contact"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Контакты с колледжем</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/connectionteachers"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Связь с учителем</router-link
+                    >
                   </li>
                 </ul>
               </li>
@@ -174,10 +374,23 @@
                   Работадателям
                 </a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
                   <li>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <router-link
+                      to="/partner"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Партнеры</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/preperationprof"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Подготовка специалистов</router-link
+                    >
                   </li>
                 </ul>
               </li>
@@ -192,10 +405,23 @@
                   Выпускникам
                 </a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
                   <li>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <router-link
+                      to="/association"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Ассоциация</router-link
+                    >
+                  </li>
+                  <li>
+                    <router-link
+                      to="/sponsorship"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Спонсорство</router-link
+                    >
                   </li>
                 </ul>
               </li>
@@ -210,14 +436,17 @@
                   Календарь
                 </a>
                 <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
                   <li>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <router-link
+                      to="/googlecalendar"
+                      exact
+                      class="dropdown-item"
+                      href="#"
+                      >Google Calendar</router-link
+                    >
                   </li>
                 </ul>
               </li>
-
               <li class="nav-item dropdown">
                 <a
                   class="nav-link dropdown-toggle active"
